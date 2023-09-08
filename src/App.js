3bc0cbbf1dc9c3bb2e11e3f79ba7df13de4a4676
tejaas07 <ktejas18@gmail.com>
2023-09-08 18:57:01 +0530

    Project Completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import "./App.css";
-import Navbar from "./components/Navbar";
-import Routing from "./components/Routing";
-import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import AboutPage from './pages/AboutPage';
+import MenuPage from './pages/MenuPage';
+import ReservationPage from './pages/ReservationPage';
+
 function App() {
   return (
-    <>
-      <Navbar />
-      <Routing />
-      <Footer />
-    </>
+    <div className='main-area'>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/about' element={<AboutPage/>}/>
+          <Route path='/menu' element={<MenuPage/>}/>
+          <Route path='/reservations' element={<ReservationPage/>}/>
+        </Routes>
+        <Footer/>
+      </Router>
+    </div>
   );
 }
 
